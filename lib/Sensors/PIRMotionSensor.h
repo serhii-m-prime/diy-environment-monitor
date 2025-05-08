26abe904253173573431e357c71a36783c0270e8
Serhii <s.maxymenko@gmail.com>
2025-05-08 20:07:55 +0300

    Added motion sensor implementation

---
+++ lib/Sensors/PIRMotionSensor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "IEventSensor.h"
+#include <Arduino.h>
+
+class PIRMotionSensor : public IEventSensor
+{
+public:
+PIRMotionSensor(int pin);
+    void begin() override;
+    void update() override;
+    void onEvent(std::function<void()> callback) override;
+    void onEventStart(std::function<void()> callback) override;
+    void onEventEnd(std::function<void()> callback) override;
+    void setDebounceDelay(unsigned long time) override;
+
+private:
+    uint8_t _pin;
+    std::function<void()> _callback;
+    std::function<void()> _startCallback;
+    std::function<void()> _endCallback;
+    int _lastState;
+    int _currentState;
+    unsigned long _lastDebounceTime;
+    unsigned long _debounceDelay;
+};
